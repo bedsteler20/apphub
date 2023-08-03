cc0bea1404f7cbae85f5b623d0953174990be3f4
Cameron Dehning <bedsteler2.0@gmail.com>
2023-08-03 15:47:20 -0500

    CSpell configured

--- bin/apphub.in.py
+++ bin/apphub.in.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env @PYTHON@
+# cSpell: enableCompoundWords
 import os
 import sys
 import signal
@@ -6,21 +7,22 @@ import locale
 import gettext
 
 
-VERSION = '@VERSION@'
-pkgdatadir = '@pkgdatadir@'
-localedir = '@localedir@'
-sourceroot = '@sourceroot@'
+VERSION = "@VERSION@"
+pkgdatadir = "@pkgdatadir@"
+localedir = "@localedir@"
+sourceroot = "@sourceroot@"
 
 sys.path.insert(1, pkgdatadir)
 signal.signal(signal.SIGINT, signal.SIG_DFL)
-locale.bindtextdomain('apphub', localedir)
-locale.textdomain('apphub')
-gettext.install('apphub', localedir)
+locale.bindtextdomain("apphub", localedir)
+locale.textdomain("apphub")
+gettext.install("apphub", localedir)
 
 if "@build_type@" == "debug":
     print("Running in debug mode")
     try:
         import debugpy
+
         debugpy.connect(5678)
         print("Waiting for debugger connection ...")
         debugpy.wait_for_client()
@@ -30,17 +32,21 @@ if "@build_type@" == "debug":
     except ConnectionRefusedError:
         print("Unable to connect to the debugger Connection was Refused")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import gi
-    gi.require_versions({
-        'Flatpak': '1.0',
-        'Gtk': '4.0',
-        'Adw': '1',
-    })
+
+    gi.require_versions(
+        {
+            "Flatpak": "1.0",
+            "Gtk": "4.0",
+            "Adw": "1",
+        }
+    )
     from gi.repository import Gio
-    resource = Gio.Resource.load(os.path.join(
-        pkgdatadir, 'apphub.gresource'))
+
+    resource = Gio.Resource.load(os.path.join(pkgdatadir, "apphub.gresource"))
     resource._register()
 
     from apphub import main
+
     sys.exit(main.main(VERSION))
