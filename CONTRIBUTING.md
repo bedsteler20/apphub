2730a3463d2261b8c715986987dba835be111195
Cameron Dehning <bedsteler2.0@gmail.com>
2023-08-04 12:49:11 -0500

    add github actions

---
+++ CONTRIBUTING.md
@@ -0,0 +1,9 @@
+# Install dependencies
+#### Install devtools and type stubs
+```bash
+pip install -r requirements.txt
+```
+#### Install runtime dependencies
+```bash
+pip install -r build-aux/requirements.txt
+```
--- apphub/utils/flatpak.py
+++ apphub/utils/flatpak.py
@@ -2,8 +2,7 @@ import os
 from enum import Enum
 
 from gi.repository import Adw, Flatpak, Gio, GLib, GObject
-from gi.repository.Flatpak import (Installation, TransactionOperation,
-                                   TransactionResult)
+from gi.repository.Flatpak import Installation, TransactionOperation, TransactionResult
 
 from apphub.utils.locate import locate
 
