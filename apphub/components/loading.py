a0bf5f378f7ee6cd7a538465885a59a04701c011
Cameron Dehning <bedsteler2.0@gmail.com>
2023-12-29 13:23:15 -0600

    Prep for merge of rust1

--- apphub/components/loading.py
+++
@@ -1,9 +0,0 @@
-from gi.repository import GObject, Gtk
-
-
-@Gtk.Template(resource_path="/com/bedsteler20/AppHub/loading.ui")
-class LoadingWidget(Gtk.Box):
-    __gtype_name__ = "LoadingWidget"
-    loading: bool = GObject.Property(
-        type=bool, default=False, flags=GObject.ParamFlags.READWRITE
-    )
